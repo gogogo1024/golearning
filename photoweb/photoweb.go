22f280d7c4f657b4978fdcb96054325a2d72db4f
gogogo1024 <jxycbjhc@163.com>
2021-10-02 09:32:32 +0800

    添加photoweb项目 上传图片 浏览图片 图片列表

---
+++ photoweb/photoweb.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"html/template"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+)
+const (UPLOAD_DIR = "./uploads")
+// 上传文件
+func uploadHandler(w http.ResponseWriter, r *http.Request)  {
+	if r.Method == "GET"{
+	 if err := rendHtml(w,"upload",nil); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+		}
+	}
+	if r.Method == "POST"{
+		f,h,err := r.FormFile("image")
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		filename :=h.Filename
+		defer f.Close()
+		t, err :=os.Create(UPLOAD_DIR+"/"+filename)
+		if err != nil {
+			http.Error(w,err.Error(),http.StatusInternalServerError)
+			return
+		}
+		defer t.Close()
+		if _,err :=io.Copy(t,f); err != nil {
+			http.Error(w,err.Error(),http.StatusInternalServerError)
+			return
+		}
+		http.Redirect(w,r,"/view?id="+filename,http.StatusFound)
+	}
+}
+
+// 浏览文件
+func viewHandler(w http.ResponseWriter,r *http.Request)  {
+	imageId := r.FormValue("id")
+	imagePath := UPLOAD_DIR+"/"+imageId
+	if exists :=isExists(imagePath); exists {
+		http.NotFound(w,r)
+		return
+	}
+	w.Header().Set("Content-type","image")
+	http.ServeFile(w,r,imagePath)
+
+}
+
+func isExists(path string)bool  {
+	_, err:=os.Stat(path)
+	if err != nil {
+		return true
+	}
+	return os.IsExist(err)
+}
+
+func  listHandler(w http.ResponseWriter, r *http.Request)  {
+	fileInfoArr, err:=ioutil.ReadDir(UPLOAD_DIR)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	locals := make(map[string]interface{})
+	images :=[] string {}
+
+	for _,fileInfo := range fileInfoArr{
+		images =append(images,fileInfo.Name())
+	}
+	locals["images"] = images
+	if err =rendHtml(w,"list",locals); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+func rendHtml(w http.ResponseWriter,tmpl string, locals map[string]interface{}) error {
+	t,err:= template.ParseFiles(tmpl + ".html")
+	if err != nil {
+		return err
+	}
+	err = t.Execute(w,locals)
+	return err
+}  
+func main() {
+	http.HandleFunc("/", listHandler)
+	http.HandleFunc("/upload", uploadHandler)
+	http.HandleFunc("/view", viewHandler)
+
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ",err.Error())
+	}
+}  
